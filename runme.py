d44c9694e118a15ff9eab369b3bd6dae6977235c
Héctor Sab <hector.sab@hotmail.com>
2018-04-08 20:34:07 -0500

    .

--- runme.py
+++ runme.py
@@ -6,6 +6,7 @@ Description: Main script to run grabcut3.py
 """
 
 import os
+import numpy as np
 
 from grabcut import GrabCut
 
@@ -14,8 +15,19 @@ if __name__=='__main__':
   out_dir = '../1803_010203_01/GOPR7860_PPL_MATT/'
   files = os.listdir(ims_dir)
   files.sort()
-  for file in files:
-    print('File: ',file)
+  
+  files = np.array(files)
+  num_files = files.shape[0]
+  num2take = int(num_files*.2)
+
+  ind = np.arange(num_files)
+  np.random.shuffle(ind)
+  files[ind] = files
+  files = files[:num2take]
+  files = list(files)
+
+  for i,file in enumerate(files):
+    print('File {0}/{1}: {2}'.format(i,num2take,file))
     gc = GrabCut()
     gc.load_image(ims_dir+file)
     gc.set_output(direct=out_dir,name=file[:-4])
