ed7529ad858f86385d818c44ad34a1c47635ff92
Héctor Sab <hector.sab@hotmail.com>
2018-04-09 10:44:38 -0500

    .

--- runme.py
+++ runme.py
@@ -11,8 +11,8 @@ import numpy as np
 from grabcut import GrabCut
 
 if __name__=='__main__':
-  ims_dir = '../1803_010203_01/GOPR7860_PPL/'
-  out_dir = '../1803_010203_01/GOPR7860_PPL_MATT/'
+  ims_dir = '../1803_010203_01/GOPR7863_PPL/'
+  out_dir = '../1803_010203_01/GOPR7863_PPL_MATT/'
   files = os.listdir(ims_dir)
   files.sort()
   
